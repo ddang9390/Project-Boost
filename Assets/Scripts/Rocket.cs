a5c422610b4c1f9dd21d66ee9bd74b8948882dd6
ddang9390 <dnld1ng@yahoo.com>
2018-06-17 10:19:30 -0400

    Changed name of audio variable

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
     Rigidbody rigidBody;
-    AudioSource audio;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = this.GetComponent<Rigidbody>();
-        audio = this.GetComponent<AudioSource>();
+        audioSource = this.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -24,14 +24,14 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
-            if (!audio.isPlaying) // so it doesn't layer on top of each other
+            if (!audioSource.isPlaying) // so it doesn't layer on top of each other
             {
-                audio.Play();
+                audioSource.Play();
             }
         }
-        else if (audio.isPlaying)
+        else if (audioSource.isPlaying)
         {
-            audio.Stop();
+            audioSource.Stop();
         }
         // Rotate ship
         if (Input.GetKey(KeyCode.A))
